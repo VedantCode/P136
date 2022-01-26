7d43366904f4fec943814feac4d01d8dcbfd9b0e
VedantCode <82620879+VedantCode@users.noreply.github.com>
2022-01-25 20:28:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+video = "";
+
+function preload(){
+    video = createCapture(VIDEO);
+    video.hide();
+    video.size(280, 280)
+}
+
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    video.loop();
+    video.speed(1);
+    video.volume();
+}
